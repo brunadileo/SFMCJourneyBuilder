f944072547ffa316dae9cd60c1329cc6ec39b10b
brunadileo <brunagdileo@gmail.com>
2018-09-20 14:57:45 -0300

    changes

--- public/js/testsms.js
+++ public/js/testsms.js
@@ -1,12 +1,13 @@
 
-const accountSid = process.env.accountSid; console.log(accountSid);
-const authToken = process.env.authToken; console.log(authToken);
+const accountSid = process.env.ACCOUNTSID; console.log(accountSid);
+const authToken = process.env.AUTHTOKEN; console.log(authToken);
+
 const client = require('twilio')(accountSid, authToken); 
 
     client.messages.create({
         from: '+17853766431',
         to: '+5521981248459',
-        body: "Olar"
+        body: "You just sent an SMS from Node.js using Twilio!"
         
     }).then(message => console.log(message.sid)) 
     .done();
